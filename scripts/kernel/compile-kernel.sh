34414583a6203aed20082d2c8e752fa6d5918cdb
Willy Sudiarto Raharjo <willysr@gmail.com>
2012-07-17 18:15:40 +0700

    Update Linux Kernel Config for 3.4.5

--- scripts/kernel/compile-kernel.sh
+++ scripts/kernel/compile-kernel.sh
@@ -8,7 +8,7 @@
 # Initial script by Robby Workman <http://rlworkman.net/>
 # Slightly modified by Willy Sudiarto Raharjo <willysr@gmail.com>
 #
-VERSION='3.3' # change to reflect actual kernel version
+VERSION='3.4.5' # change to reflect actual kernel version
 CWD='/usr/src/' # /usr/src directory
 #
 # Remove /usr/src/linux symlink
