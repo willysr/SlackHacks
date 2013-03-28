4cebc0c26f982bf65c32012b3882c17aa8f22b8e
Willy Sudiarto Raharjo <willysr@gmail.com>
2013-03-28 14:34:13 +0700

    brainworkshop: Rebuilt to add parameter --datadir
    Signed-off-by: Willy Sudiarto Raharjo <willysr@gmail.com>

--- SlackBuilds/brainworkshop/brainworkshop.sh
+++ SlackBuilds/brainworkshop/brainworkshop.sh
@@ -2,5 +2,6 @@
 
 BRW_CONFIGFILE=$HOME/.brainworkshop.ini
 BRW_STATFILE=$HOME/.brainworkshop.stats
+BRW_DATADIR=$HOME/brainworkshop
 
-python2.7 /usr/share/brainworkshop/brainworkshop.pyw --configfile $BRW_CONFIGFILE --statsfile $BRW_STATFILE
+python2.7 /usr/share/brainworkshop/brainworkshop.pyw --configfile $BRW_CONFIGFILE --statsfile $BRW_STATFILE --datadir $BRW_DATADIR
