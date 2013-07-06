ab172348541d20e394fc9483bb2eea5c651667ae
Willy Sudiarto Raharjo <willysr@slackware-id.org>
2013-07-06 23:25:30 +0700

    Update Kernel Compile Script to 3.9.9
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackware-id.org>

--- scripts/kernel/compile-kernel.sh
+++ scripts/kernel/compile-kernel.sh
@@ -8,7 +8,7 @@
 # Initial script by Robby Workman <http://rlworkman.net/>
 # Slightly modified by Willy Sudiarto Raharjo <willysr@gmail.com>
 #
-VERSION='3.9.7' # change to reflect actual kernel version
+VERSION='3.9.9' # change to reflect actual kernel version
 CWD='/usr/src' # /usr/src directory
 cd $CWD
 #
