99f08745ee106a59484974b4f994df2ba7536f2c
Willy Sudiarto Raharjo <willysr@slackware-id.org>
2013-09-17 18:42:54 +0700

    Update Compile Kernel Script
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackware-id.org>

--- scripts/kernel/compile-kernel.sh
+++ scripts/kernel/compile-kernel.sh
@@ -8,7 +8,7 @@
 # Initial script by Robby Workman <http://rlworkman.net/>
 # Slightly modified by Willy Sudiarto Raharjo <willysr@gmail.com>
 #
-VERSION='3.11' # change to reflect actual kernel version
+VERSION='3.11.1' # change to reflect actual kernel version
 CWD='/usr/src' # /usr/src directory
 cd $CWD
 #
