13bc7f5b577031fe29605a73e01f6ad2566c8db6
Willy Sudiarto Raharjo <willysr@slackware-id.org>
2017-07-20 20:48:40 +0700

    compile-kernel: Updated to 4.9.38.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackware-id.org>

--- scripts/kernel/compile-kernel.sh
+++ scripts/kernel/compile-kernel.sh
@@ -8,7 +8,7 @@
 # Initial script by Robby Workman <http://rlworkman.net/>
 # Slightly modified by Willy Sudiarto Raharjo <willysr@gmail.com>
 #
-VERSION='3.11.6' # change to reflect actual kernel version
+VERSION='4.9.38' # change to reflect actual kernel version
 CWD='/usr/src' # /usr/src directory
 cd $CWD
 #
